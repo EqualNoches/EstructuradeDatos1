aec32a0a7a291fa6c0fd8c376a58c8868219fd76
HectorWittkop <143217990+HectorWittkop@users.noreply.github.com>
2023-09-08 18:36:48 -0400

    Update Linked.cpp
    Con metodos agregados

--- tareas/ListasEnlazadas/Linked.cpp
+++ tareas/ListasEnlazadas/Linked.cpp
@@ -1,215 +1,230 @@
 
 
 #include <iostream>
-#include<stdlib.h>
+#include <stdlib.h>
 using namespace std;
 
-//Estructura de lista
-struct LinkedList
+struct Nodo
 {
-	int dato;
-	LinkedList *next;
+    int dato;
+    Nodo* next;
 };
 
-//Prototipos de funcion
-void Insertar(LinkedList *&, int);
-void Mostrar(LinkedList*);
+Nodo* lista = NULL;
+void Insertar(Nodo *&, int n);
+void Mostrar(Nodo *);
+void Buscar(Nodo *, int n);
+void eliminarElemento(Nodo*&, int n);
+void eliminarLista(Nodo*&, int&);
+void ValidarElemento(int &);
 void Menu();
-void Buscar(LinkedList*, int);
-void Eliminar(LinkedList*&, int);
-
-//Hacemos lista una variable global
-LinkedList* lista = NULL;
 
 int main()
 {
-	Menu();
-
-	return 0;
+    Menu();
+    return 0;
 }
 
-//Metodo de insercion ordenado
-void Insertar(LinkedList*& lista, int n) 
-{
-	LinkedList *nuevo = new LinkedList();
-	nuevo->dato = n;
-
-	LinkedList* aux1 = lista;
-	LinkedList* aux2 = NULL;
-
-	while ((aux1 != NULL) && (aux1->dato < n)) //Este bucle sirve para recorrer de manera ordenada la lista
-	{
-		aux2 = aux1;
-		aux1 = aux1->next;
-	}
-
-	if (lista == aux1)//Este condicional se ejecuta cuando insertamos un elemento al principio
-	{
-		lista = nuevo;
-		
-	}
-	else //Si ejecutamos este condicional significa que ya entramos al while y por ende recorrimos una posicion
-	{
-		aux2->next = nuevo;
-
-	}
-	nuevo->next = aux1;
-
-	cout << "\tElemento " << n << " Insertado correctamente\n";
-
-}
-
-//Funcion para mostrar la lista en pantalla
-void Mostrar(LinkedList* lista) 
-{
-	LinkedList* actual = new LinkedList();
-	actual = lista;
-
-	while (actual != NULL)//Este bucle nos sirve para imprimir al mismo tiempo en que recorremos la lista.
-	{
-		cout << actual->dato << " -> ";
-		actual = actual->next;
-	}
-	if (lista == NULL)
-	{
-		cout << "La lista esta vacia\n";
-	}
-}
-
-//Metodo del menu
 void Menu() 
 {
-	int opc;
-	int dato;
+    int opcion, dato;
+    do
+    {
+        cout << "\t.:Menu de Listas Enlazadas:.\n";
+        cout << "1.Insertar un elemento de la lista\n";
+        cout << "2.Mostar los elementos de la lista\n ";
+        cout << "3.Buscar un elemento de la lista\n";
+        cout << "4.Eliminar un elemento de la lista\n";
+        cout << "5.Eliminar la lista\n";
+        cout << "6.Salir\n";
 
-	do
-	{
-		cout << "\tMENU\n";
-		cout << "1.Insertar elementos a la lista\n";
-		cout << "2.Mostrar elementos de la lista \n";
-		cout << "3.Buscar un elemento de la lista \n";
-		cout << "4.Eliminar un elemento de la lista\n";
-		cout << "5.Salir\n";
-		cout << "\n";
-		cout << "Digite la opcion que desea ejecutar: ";
-		cin >> opc;
+        cout << " Opcion: ";
+        cin >> opcion;
 
-		switch (opc)
-		{
-		case 1:
-			cout << "Inserta un numero: ";
-			cin >> dato;
-			Insertar(lista, dato);
-			cout << "\n";
-			system("pause");
-			break;
-		case 2:
-			Mostrar(lista);
-			cout << "\n";
-			system("pause");
-			break;
-		case 3:
-			if (lista == NULL)
-			{
-				cout << "La lista esta vacia\n";
-				system("pause");
-			}
-			else
-			{
-				cout << "Digite el numero que desea buscar en la lista: ";
-				cin >> dato;
-				Buscar(lista, dato);
-				cout << "\n";
-				system("pause");
-			}
-			break;
-		case 4:
-			if (lista == NULL)
-			{
-				cout << "La lista esta vacia\n";
-				system("pause");
-			}
-			else
-			{
-				cout << "Digite el numero que desea eliminar de la lista: ";
-				cin >> dato;
-				Eliminar(lista, dato);
-				cout << "\n";
-				system("pause");
-				break;
-			}
-			
-		}
+        switch (opcion)
+        {
+        case 1:
+            cout << "Inserta un numero: ";
+            ValidarElemento(dato);
+            Insertar(lista, dato);
+            cout << "\n";
+            system("pause");
+            break;
+        case 2:
+            if (lista == NULL) 
+            {
+                cout << "La lista esta vacia\n";
+                system("pause");
+            }
+            else
+            {
+                Mostrar(lista);
+                cout << "\n";
+                system("pause");
+            }
+           
+            break;
+        case 3:
+            if (lista == NULL)
+            {
+                cout << "La lista esta vacia\n";
+                system("pause");
+            }
+            else
+            {
+                cout << "Digite el numero que desea buscar en la lista: ";
+                ValidarElemento(dato);
+                Buscar(lista, dato);
+                cout << "\n";
+                system("pause");
+            }
+            break;
+        case 4:
+            if (lista == NULL)
+            {
+                cout << "La lista esta vacia\n";
+                system("pause");
+            }
+            else
+            {
+                cout << "Digite el numero que desea eliminar de la lista: ";
+                ValidarElemento(dato);
+                eliminarElemento(lista, dato);
+                cout << "\n";
+                system("pause");
+                break;
+            }
+        case 5: while (lista != NULL)
+        {
+            eliminarLista(lista, dato);
+            cout << dato << " -> ";
+        }
+              cout << "\n";
+              system("pause");
+            break;
 
-		system("cls");
-	} while (opc != 5);
+        }
 
+        system("cls");
+        
+        
+    } while (opcion != 6);
 }
 
-//Metodo para buscar elementos en la lista
-void Buscar(LinkedList* lista, int n) 
+void Insertar(Nodo *&lista,int n) 
 {
-	bool band = false; //Esta variable no sirve para determinar si encontramos el elemento
+    Nodo *nuevo_nodo = new Nodo();
+    nuevo_nodo->dato = n;
 
-	LinkedList* actual = new LinkedList();
-	actual = lista;
+    Nodo *aux1 = lista;
+    Nodo *aux2 = NULL;
 
-	while ((actual != NULL) && (actual->dato <= n))//Recorremos la lista con el bucle
-	{
-		if (actual->dato == n)//Si esta condicion se cumple encontramos el elemento
-		{
-			band = true;
-		}
-		actual = actual->next;
-	}
-	if (band == true)
-	{
-		cout << "Elemento " << n << " HA sido encontrado en la lista\n";
-	}
-	else
-	{
-		cout << "Elemento " << n << " NO existe en la lista\n";
-	}
-
-
-
+    while ((aux1 != NULL) && (aux1->dato < n))
+    {
+        aux2 = aux1;
+        aux1 = aux1->next;
+    }
+    if (lista == aux1)
+    {
+        lista = nuevo_nodo;
+    }
+    else
+    {
+        aux2 -> next = nuevo_nodo;
+    }
+    
+    nuevo_nodo->next = aux1;
+    
+    cout << "\tElemento " << n << " insertado a la lista correctamente\n";
 }
 
-void Eliminar(LinkedList*& lista, int n) 
+void Mostrar(Nodo*lista) 
 {
+    Nodo* actual = new Nodo();
 
-	if (lista != NULL)//Si esta condicion no se cumple significa que la lista esta vacia por ende todo el codigo de esta funcion se corre dentro del condicional
-	{
-		LinkedList* aux_del;
-		LinkedList* prev = NULL;
-
-		aux_del = lista;
-
-		while ((aux_del != NULL) && (aux_del->dato != n))//Recorremos la lista en busca del valor a eliminar
-		{
-			prev = aux_del;
-			aux_del = aux_del->next;
-		}
-
-		if (aux_del == NULL)
-		{
-			cout << "Elemento no encontrado en la lista";
-		}
-		else if (prev == NULL)//Si esta condicion se cumple signfica que estamos eliminando el primer elemento de la lista
-		{
-			lista = lista->next;
-			delete aux_del;
-			cout << "El elemento " << n << " Ha sido eliminado de la lista";
-		}
-		else
-		{
-			prev->next = aux_del->next;
-			delete aux_del;
-			cout << "El elemento " << n << " Ha sido eliminado de la lista";
-		}
-
-
-
-	}
-
+    actual = lista;
+    while (actual != NULL)
+    {
+        cout << actual->dato << " -> ";
+        actual = actual->next;
+    }
 }
+
+void Buscar(Nodo *lista, int n) 
+{
+    bool found = false;
+
+    Nodo* actual = new Nodo();
+    actual = lista;
+
+    while ((actual != NULL) && (actual->dato <= n))
+    {
+        if (actual->dato == n)
+        {
+            found = true;
+        }
+        actual = actual->next;
+
+    }
+
+    if (found == true)
+    {
+        cout << "Elemento " << n << " ha sido encontrado\n";
+    }
+    else
+    {
+        cout << "Elemento " << n << " no ha sido encontrado\n";
+    }
+}
+
+void eliminarElemento(Nodo *&lista, int n) 
+{
+    if (lista != NULL)
+    {
+        Nodo* aux_borrar;
+        Nodo* anterior = NULL;
+        aux_borrar = lista;
+
+        while ((aux_borrar != NULL) && (aux_borrar->dato != n))
+        {
+            anterior = aux_borrar;
+            aux_borrar = aux_borrar->next;
+        }
+
+        if (aux_borrar == NULL)
+        {
+            cout << "El elemento no existe";
+        }
+        else if (anterior == NULL)
+        {
+            lista = lista->next;
+            delete aux_borrar;
+        }
+        else
+        {
+            anterior->next = aux_borrar->next;
+            delete aux_borrar;
+        }
+
+    }
+}
+
+void eliminarLista(Nodo *&lista, int &n) 
+{
+    Nodo* aux = lista;
+    n = aux->dato;
+    lista = aux->next;
+    delete aux;
+}
+
+void ValidarElemento(int &n) 
+{
+    while (!(cin >> n))
+    {
+        cout << "Porfavor introduzca un valor valido";
+        cin.clear();
+        cin.ignore(123, '\n');
+    }
+}
+
+

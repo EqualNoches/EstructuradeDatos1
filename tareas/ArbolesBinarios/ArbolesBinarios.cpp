1f1178e1178afc48081d26e3a1afbf307dceee7f
EqualNoches <1115808@est.intec.edu.do>
2023-09-22 16:56:13 -0400

    codigo presentacion

--- tareas/ArbolesBinarios/ArbolesBinarios.cpp
+++ tareas/ArbolesBinarios/ArbolesBinarios.cpp
@@ -1,3 +1,36 @@
+/*
+Tarea 7. Gestión de un Árbol Binario de Búsqueda.
+Fecha de Entrega: 22 de Septiembre 2022, horario de Laboratorio 20/22
+
+Realizar un programa C++ que permita gestionar un Árbol Binario de Búsqueda. El programa 
+debe permitir insertar, buscar y eliminar un nodo; además de presentar (utilizando cualquiera 
+de los recorridos) el árbol de acuerdo a las reglas que existen para el árbol binario de búsqueda.
+
+RESTRICCIONES:
+    A. El primer nodo siempre será el root.
+    B. Al presentar el árbol debe ser de un modo intuitivo, que muestre la topología de forma 
+        que represente en árbol. 
+    C. Puede utilizar cualquiera de los recorridos para arboles (InOrden, PreOrden o 
+        PostOrden).
+    D. Al eliminar un nodo debe observar las reglas para la sustitución del nodo, permitiendo 
+        así que el árbol se reconstruya.
+    E. Al insertar un nodo deben observarse las reglas para arboles binarios de búsqueda, es 
+        decir, los nodos cuyo valor sea mayor que el nodo raíz a la derecha (en el subárbol 
+        derecho), en caso contrario a la izquierda (en el subárbol izquierdo). Recordando que 
+        deben ocupar el lugar que le corresponda según la topología del árbol.
+
+Participantes:
+ #. Name - ID
+
+ 1. Edward Díaz - ID:1115808
+ 2. Hector Wittkop - ID:1115754
+ 3. Alexander Gil - ID:1116763
+ 4. Juan Ortiz - ID:1115143
+ 5. Jose de la Cruz - ID:1113682
+ 6. Erick Saviñon - ID:1115161
+
+*/
+
 #include <iostream>
 #include <stdlib.h>
 
@@ -140,6 +173,16 @@ Node *FindMin(Node *root)
     while (actual->izquierda != NULL)
     {
         actual = actual->izquierda;
+    }
+    return actual;
+}
+
+Node *FindMax(Node *root)
+{
+    Node *actual = root;
+    while (actual->derecha != NULL)
+    {
+        actual = actual->derecha;
     }
     return actual;
 }
